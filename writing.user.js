3445650f3b7cb2f5eb40153dae4f1c13bc41c61d
Capricom <ybitschnau@gmail.com>
2024-03-13 22:47:17 +0100

    Update writing.user.js
    Detect timeout page by tab title instead of vimeo URL

--- writing.user.js
+++ writing.user.js
@@ -12,7 +12,7 @@
 /* eslint-env jquery */
 //Refresh a "Heavy server volume" page half a second after it loads
 
-var test = /vimeo.com\/video\/892103601/.test(document.body.innerHTML);
+var test = document.title === "One Moment Please...";
 if (document.location.pathname.match(/\/main\/interactive-story\/item_id\//) && test)
 {
     setTimeout(function() {
